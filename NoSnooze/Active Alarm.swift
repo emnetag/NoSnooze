fc6efeae0d03566e5c5fb3ea3a40b349573f52ed
Vikram <coolvikram@msn.com>
2015-12-11 16:47:18 -0800

    added alarm page

--- NoSnooze/Active Alarm.swift
+++ NoSnooze/Active Alarm.swift
@@ -17,6 +17,10 @@ class Active_Alarm: UIViewController {
     @IBOutlet weak var thatime: UILabel!
     @IBOutlet var readyUp: UIButton!
     @IBOutlet weak var countdown: UIButton!
+    var timer = NSTimer() //make a timer variable, but don't do anything yet
+    let timeInterval:NSTimeInterval = 0.05
+    let timerEnd:NSTimeInterval = 1000.0
+    var timeCount:NSTimeInterval = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
         let audioFilePath = NSBundle.mainBundle().pathForResource("alarm", ofType: "mp3")
@@ -31,7 +35,40 @@ class Active_Alarm: UIViewController {
         }
         
     }
-    @IBAction func startCountdown(sender: AnyObject) {
+    func resetTimeCount(){
+        timeCount = timerEnd
+    }
+    @IBAction func resetTimer(sender: UIButton) {
+        timer.invalidate()
+        resetTimeCount()
+        thatime.text = timeString(timeCount)
+    }
+    @IBAction func startTimer(sender: UIButton) {
+        if !timer.valid{ //prevent more than one timer on the thread
+            thatime.text = timeString(timeCount)
+            timer = NSTimer.scheduledTimerWithTimeInterval(timeInterval,
+                target: self,
+                selector: "timerDidEnd:",
+                userInfo: "Pizza Done!!",
+                repeats: true) //repeating timer in the second iteration
+        }
+    }
+    
+    func timeString(time:NSTimeInterval) -> String {
+        let minutes = Int(time) / 60
+        //let seconds = Int(time) % 60
+        let seconds = time - Double(minutes) * 60
+        let secondsFraction = seconds - Double(Int(seconds))
+        return String(format:"%02i:%02i.%01i",minutes,Int(seconds),Int(secondsFraction * 10.0))
+    }
+    func timerDidEnd(timer:NSTimer){
+        timeCount = timeCount - timeInterval
+        if timeCount <= 0 {
+            thatime.text = "Pizza Ready!!"
+            timer.invalidate()
+        } else { //update the time on the clock if not reached
+            thatime.text = timeString(timeCount)
+        }
     }
     @IBAction func snoozePressed(sender: AnyObject) {
         audioPlayer.stop()
